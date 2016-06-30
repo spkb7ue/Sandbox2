cb5d78cc304d64bdff0ef1c9472c8f313c46fed9
spkb7ue <spkb7ue@gmail.com>
2016-06-30 19:06:13 +0800

    tested reading file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+struct Point{
+    float x,y,z;
+};
 
 int main()
 {
 	cout<<"Hellp\n";
+	ifstream infile;
+    infile.open("rabbit.triangles");
+    std::vector<Point> points;
+
+    Point tmp;
+    while(infile >> tmp.x >> tmp.y >> tmp.z)
+    {
+        points.push_back(tmp);
+    }
+    infile.close();
+    cout<<points.size()<<endl;
 }
