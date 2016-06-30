4d471024d3148d1907c93a7978229ee9d7a4122a
spkb7ue <spkb7ue@gmail.com>
2016-06-30 22:18:01 +0800

    clean up

--- Mesh.h
+++ Mesh.h
@@ -2,18 +2,20 @@
 
 #include <vector>
 #include <string>
+#include <boost/noncopyable.hpp>
 
 #include "Triangle.h"
 
 namespace rabbit
 {
-    class Mesh
+    class Mesh : boost::noncopyable
     {
     public:
+
         explicit Mesh(std::string fileName);
 
-        std::vector<Triangle>& GetTriangles();
 
+        std::vector<Triangle>& GetTriangles();
     private:
         std::vector<Triangle> m_triangles;
 
