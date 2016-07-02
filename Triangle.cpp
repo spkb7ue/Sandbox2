1f0839f897d62d18de6283be9d5096a930512d15
spkb7ue <spkb7ue@gmail.com>
2016-07-02 15:16:58 +0800

    back up

--- Triangle.cpp
+++ Triangle.cpp
@@ -9,7 +9,23 @@ Triangle::Triangle(const Point& p0, const Point& p1, const Point& p2)
  P2(p2),
  P0_P1(p1-p0),
  P0_P2(p2-p0),
+ P1_P2(p2-p1),
  Normal((Vec3::crossProduct(P0_P1,P0_P2)).normalise())
 {
 
+}
+
+bool Triangle::IsPointWithinExtrudedTriangle(const Point& p)
+{
+
+    return true;
+}
+
+std::pair<Point,float> Triangle::ProjectPointOntoTrianglePlane(const Point& p)
+{
+    const auto p_P0 = P0-p;
+    auto signedDist = Vec3::dotProduct(p_P0, Normal);
+
+
+    return std::make_pair(Point(p + Normal*signedDist), signedDist);
 }
--- Triangle.h
+++ Triangle.h
@@ -9,11 +9,14 @@ namespace rabbit
         Point P0;
         Point P1;
         Point P2;
-        Vec3 P0_P1; ///< This is P0 to P1 vector
-        Vec3 P0_P2; ///< This is the P0 to P2 vector;
+        Vec3 P0_P1; ///< P0 to P1 Vector
+        Vec3 P0_P2; ///< P0 to P2 Vector;
+        Vec3 P1_P2; ///< P1 to P2 Vector
         Vec3 Normal; ///< Normal to the Triangle's plane
 
         bool IsPointWithinExtrudedTriangle(const Point& p);
+
+        std::pair<Point,float> ProjectPointOntoTrianglePlane(const Point& p);
 
         Triangle(const Point& p0, const Point& p1, const Point& p2);
     };
