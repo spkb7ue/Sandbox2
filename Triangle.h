0df801715b111f7e79008f386e0ffe2634cf4544
spkb7ue <spkb7ue@gmail.com>
2016-07-03 23:33:00 +0800

    more refactor

--- Triangle.h
+++ Triangle.h
@@ -25,7 +25,7 @@ class Tri
 {
 public:
     typedef typename IsPropertyModifiable<T,isTriangleDeformable>::type VertType;
-    typedef typename IsPropertyModifiable<T,isTriangleDeformable>::type EdgeType;
+    typedef typename IsPropertyModifiable<T,isTriangleDeformable>::type VecType;
 
     enum Edge
     {
@@ -34,24 +34,32 @@ public:
         eP1P2 = 2   ///< Vector from P1 to P2
     };
 
+    enum Vert
+    {
+        eP0 = 0,    ///< Refers to Vertex P0
+        eP1 = 1,    ///< Refers to Vertex P1
+        eP2 = 2     ///< Refers to Vertex P2
+    };
+
     Tri(const T& v0, const T& v1, const T& v2):
         m_verts({v0, v1, v2}),
         m_edges({m_verts[1]-m_verts[0],
                  m_verts[2]-m_verts[0],
-                 m_verts[2]-m_verts[1]})
-                 {}
+                 m_verts[2]-m_verts[1]}),
+        m_normal(Vec3::crossProduct(m_edges[eP0P1],m_edges[eP0P2]).normalise()){}
 
-    VertType& Vert(unsigned index){
-        return m_verts[index];
+    VertType& Vert(Vert vertID){
+        return m_verts[static_cast<short>(vertID)];
     }
 
-    EdgeType& Edge(Edge edgeID){
-        return m_edges[static_cast<unsigned>(edgeID)];
+    VecType& Edge(Edge edgeID){
+        return m_edges[static_cast<short>(edgeID)];
     }
 
     static const short NUM_VERTS = 3;
     VertType m_verts[NUM_VERTS];
-    EdgeType m_edges[NUM_VERTS];
+    VecType m_edges[NUM_VERTS];
+    VecType m_normal;
 
 };
 
