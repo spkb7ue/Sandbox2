0a6e70316c128277c284ee7a04f18efa908a40ef
spkb7ue <spkb7ue@gmail.com>
2016-07-05 17:12:44 +0800

    back up

--- examples/ShapeEx.cpp
+++ examples/ShapeEx.cpp
@@ -9,21 +9,34 @@ namespace
         float w,x,y,z;
     };
 
+    struct float3
+    {
+        float x,y,z;
+    };
+
     struct TrianglePropsMinimal
     {
-        enum vid :unsigned {v0 = 0, v1 = 1, v2 = 2};
-        enum eid :unsigned {e0 = 0, e1 = 1, e2 = 2};
+        enum VertIndices :unsigned {v0 = 0, v1 = 1, v2 = 2};
+        enum EdgeIndices :unsigned {e0 = 0, e1 = 1, e2 = 2};
+        struct PointIntersectionResult{};
         static const unsigned NUM_VERTICES = 3;
     };
 
-    /*
-    struct Triangle : public IShape<float4
+/*
+    // Triangle which can't be modified
+    struct rTriangle : public IShape<float4, false, rTriangle, TrianglePropsMinimal>
     {
+        rTriangle(float4& v0, float4& v1, float4& v2):IShape<float4,
+                                                             false,
+                                                             rTriangle,
+                                                             TrianglePropsMinimal>
+                                                             (v0, v1, v2){}
 
     };
 
 
-*/
+    */
+
 
 }
 
