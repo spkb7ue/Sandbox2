31f6a1f415dc4431b62bd01113bf642b1dcb0390
spkb7ue <spkb7ue@gmail.com>
2016-07-08 04:23:15 +0800

    clean up

--- examples/MeshBuilderEx.cpp
+++ examples/MeshBuilderEx.cpp
@@ -1,4 +1,5 @@
 #include <IMeshBuildingPolicy.h>
+#include <TriangularMeshBuildingPolicy.h>
 #include <Mesh.h>
 
 using namespace std;
@@ -15,7 +16,6 @@ namespace
     {
         Point3 m_verts[4];
     };
-
 }
 
 // We can create a Mesh with any custom polygon type.
@@ -29,7 +29,14 @@ class CustomShapeMeshBuildingPolicy : public IMeshBuildingPolicy<Quad>
 
 int main()
 {
-    // Here we create a mesh consisting of polygon Quad.
-    std::shared_ptr< IMeshBuildingPolicy<Quad> > quadMeshBuilder = std::make_shared<CustomShapeMeshBuildingPolicy>();
-    std::shared_ptr<Mesh<Quad>> mesh = std::make_shared<Mesh<Quad>>(quadMeshBuilder);
+    {
+        // Here we create a mesh consisting of polygon Quad.
+        std::shared_ptr< IMeshBuildingPolicy<Quad> > quadMeshBuilder = std::make_shared<CustomShapeMeshBuildingPolicy>();
+        std::shared_ptr<Mesh<Quad>> mesh = std::make_shared<Mesh<Quad>>(quadMeshBuilder);
+    }
+
+    {   // Lets do our regular triangular mesh now.
+        const std::string FILE_NAME = "rabbit.triangles"; ///< File with the triangulated mesh
+        auto triMeshBuilder = std::make_shared<TriangularMeshBuilingPolicy>(FILE_NAME);
+    }
 }
