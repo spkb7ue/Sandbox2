eee4d1272ac9a305ff3f80de43625f808f3dc424
Spaceman Spiff <spkb7ue@gmail.com>
2016-07-08 02:41:00 +0800

    more comment

--- IProximityQueries.h
+++ IProximityQueries.h
@@ -22,12 +22,22 @@ public:
 
     explicit IProximityQueries(std::shared_ptr<Mesh<PolygonType>> mesh):m_mesh(mesh){}
 
-    template <typename VertType>
+	/** 
+	* Calculates the least distance between point and a polygon within the specified threshold
+	* @param  point Point of interest
+	* @param distThreshold Distances between the polygon and the point larger than this value are ingored.
+	* @return tuple::bool is a flag which is set to true if a point was found within the threshold, false otherwise
+	*		  tuple::VertType point on polygon closet to the input point. Legal only if bool is set to true
+			  tuple::double minimum distance between point and the polygon
+	*/
+    template <typename VertType> 
 	std::tuple<VertType, double, bool> CalculateClosestPoint(const VertType& point, double distThreshold);
 
 protected:
+	// The destructor is protected to disallow a user holding a handle onto a pointer to this object
     ~IProximityQueries(){}
-    std::shared_ptr<Mesh<PolygonType>> m_mesh;
+
+    std::shared_ptr<Mesh<PolygonType>> m_mesh; ///< Polygonal mesh of interest
 };
 
 template<typename PolygonType, typename ProximityQueryMethod>
