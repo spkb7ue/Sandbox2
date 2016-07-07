a6475e3b003ad07b2336a6975b8edc511310a8ef
Spaceman Spiff <spkb7ue@gmail.com>
2016-07-08 03:05:37 +0800

    more documentat

--- IMeshBuildingPolicy.h
+++ IMeshBuildingPolicy.h
@@ -6,11 +6,20 @@
 namespace rabbit
 {
 
+/** 
+* An abstract interface to the mesh building policy. Users are expected to provide
+* their own implementation to create the mesh in different ways.
+* @tparam PolygonType Type of polygon comprising the mesh
+*/
 template<typename PolygonType>
 class IMeshBuildingPolicy// : boost::noncopyable
 {
 public:
-
+	/**
+	* @param polygons A reference to the vector of polygons. It is expected
+	* that when this function is called, the concrete class will populate
+	* the vector with polygons of type PolygoneType
+	*/
     virtual void GeneratePolygons(std::vector<PolygonType>& polygons) = 0;
 
     virtual ~IMeshBuildingPolicy(){}
--- Mesh.h
+++ Mesh.h
@@ -6,25 +6,36 @@
 namespace rabbit
 {
 
-    template<typename Polygon>
-    class IMeshBuildingPolicy;
+template<typename Polygon> class IMeshBuildingPolicy; ///< Forward declare the Mesh Builder
 
-    template<typename PolygonType>
-    class Mesh
-    {
-    public:
-        Mesh(std::shared_ptr<IMeshBuildingPolicy<PolygonType>> buildingPolicy);
+/**
+* A uniform mesh consisting of a vector of polgons. Assumption is made that
+* the mesh has all polygons of the same type. Use of templates imply we can easily
+* change the underlying shape comprising the mesh.
+* @tparam PolygonType The type polygon making up the mesh
+*/
+template<typename PolygonType>
+class Mesh
+{
+public:
+	/**
+	* @param buildingPolicy The mesh only gets an interface to the MeshBuilder.
+	* Users can easily create their own policy to create an object of this type
+	*/
+    Mesh(std::shared_ptr<IMeshBuildingPolicy<PolygonType>> buildingPolicy);
+    
+	// Access the vector of underlying polygons
+	std::vector<PolygonType>& GetPolygons(){return m_polygons;}
+    const std::vector<PolygonType>& GetPolygons()const{return m_polygons;}
 
-        std::vector<PolygonType>& GetPolygons(){return m_polygons;}
-        const std::vector<PolygonType>& GetPolygons()const{return m_polygons;}
+private:
+    std::vector<PolygonType> m_polygons;
+};
 
-    private:
-        std::vector<PolygonType> m_polygons;
-    };
+template<typename PolygonType>
+Mesh<PolygonType>::Mesh(std::shared_ptr<IMeshBuildingPolicy<PolygonType>> buildingPolicy)
+{
+    buildingPolicy->GeneratePolygons(m_polygons);
+}
 
-    template<typename PolygonType>
-    Mesh<PolygonType>::Mesh(std::shared_ptr<IMeshBuildingPolicy<PolygonType>> buildingPolicy)
-    {
-        buildingPolicy->GeneratePolygons(m_polygons);
-    }
 }
