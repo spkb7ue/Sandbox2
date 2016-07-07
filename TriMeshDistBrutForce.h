b52145727ae8b0dc982d94b2a2ed60d8c580337c
spkb7ue <spkb7ue@gmail.com>
2016-07-07 10:49:19 +0800

    back up

--- TriMeshDistBrutForce.h
+++ TriMeshDistBrutForce.h
@@ -12,6 +12,7 @@ class TriMeshBruteForce : public IProximityQueries<Triangle<Vec3>, TriMeshBruteF
 {
 public:
     TriMeshBruteForce(std::shared_ptr<Mesh<Triangle<Vec3>>> mesh);
+
     std::tuple<Vec3,double,bool> CalculateClosestPointImpl(const Vec3& point,double distThreshold);
 };
 
