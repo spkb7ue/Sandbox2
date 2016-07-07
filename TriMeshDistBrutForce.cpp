2ffa204be39d8afce7f9efa8514d923835328732
spkb7ue <spkb7ue@gmail.com>
2016-07-07 09:30:00 +0800

    brute force

--- TriMeshDistBrutForce.cpp
+++ TriMeshDistBrutForce.cpp
@@ -10,11 +10,21 @@ TriMeshBruteForce::TriMeshBruteForce(std::shared_ptr<Mesh<Triangle<Vec3>>> mesh)
 Vec3 TriMeshBruteForce::CalculateClosestPointImpl(const Vec3& point)
 {
     const std::vector<Triangle<Vec3>>& triangles = m_mesh->GetPolygons();
+    double minDist = std::numeric_limits<double>::max();
+    Vec3 closestPoint(0.0 ,0.0, 0.0);
+
+    // Check min distance to each triangle
     for(const Triangle<Vec3>& t : triangles)
     {
         IntersectionResult<Vec3> res = t.CalcShortestDistanceFrom(point);
+        if(res.Dist < minDist)
+        {
+            closestPoint = res.Point;
+            minDist = res.Dist;
+        }
     }
 
+    return closestPoint;
 }
 
 }
